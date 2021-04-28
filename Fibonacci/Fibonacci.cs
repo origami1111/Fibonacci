5b95205dce5ef812e469f27df2ec480270b0aae0
Вячеслав Островский <slslava007@gmail.com>
2021-04-28 17:11:42 +0300

    Add project files.

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        private uint length;
+        private uint n;
+        private uint m;
+
+        public Fibonacci() { }
+        public Fibonacci(uint length)
+        {
+            this.length = length;
+        }
+        public Fibonacci(uint n, uint m)
+        {
+            this.n = n;
+            this.m = m;
+        }
+
+        public void fibonacciLength()
+        {
+            uint j = 1;
+            for (uint i = 1; i <= length; i += j)
+            {
+                Console.Write(i + " ");
+                j = i - j;
+            }
+
+            Console.WriteLine();
+        }
+
+        public void fibonacciRange()
+        {
+            uint j = 1;
+            for (uint i = n; i <= m; i += j)
+            {
+                Console.Write(i + " ");
+                j = i - j;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibonacci;
+            uint n, m;
+            string[] param;
+
+            Console.WriteLine("Введите параметры поиска числ Фибоначчи");
+            Console.WriteLine("1. Диапазон чисел: <число a> <число b>");
+            Console.WriteLine("2. Длина: <число> ");
+            Console.Write(" > ");
+
+            param = Console.ReadLine().Split(' ');
+
+            if (param.Length == 1)
+            {
+                n = Convert.ToUInt32(param[0]);
+
+                fibonacci = new Fibonacci(n);
+                fibonacci.fibonacciLength();
+            }
+            else if (param.Length == 2)
+            {
+                n = Convert.ToUInt32(param[0]);
+                m = Convert.ToUInt32(param[1]);
+
+                fibonacci = new Fibonacci(n, m);
+                fibonacci.fibonacciRange();
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода параметров");
+            }
+
+            Console.WriteLine("Программа завершена");
+            Console.ReadKey();
+
+        }
+    }
+}
